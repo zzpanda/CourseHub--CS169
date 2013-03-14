92474fd77091c9c227da42f2575684c7c3011a86
cp123127 <cp123127@gmail.com>
2013-03-14 01:11:17 -0700

    db/schema.rb is apparently supposed to be in vc.
    Also schema is synchronized with current database.

---
+++ db/schema.rb
@@ -0,0 +1,74 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20130313224449) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "resource_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "coursems", :force => true do |t|
+    t.string   "professor"
+    t.integer  "course_id"
+    t.integer  "semester_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "coursems_users", :id => false, :force => true do |t|
+    t.integer "coursem_id", :null => false
+    t.integer "user_id",    :null => false
+  end
+
+  add_index "coursems_users", ["coursem_id", "user_id"], :name => "index_coursems_users_on_coursem_id_and_user_id"
+
+  create_table "courses", :force => true do |t|
+    t.string   "name",          :null => false
+    t.text     "course_info"
+    t.string   "department"
+    t.integer  "course_number"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "resources", :force => true do |t|
+    t.string   "resourceType"
+    t.string   "name"
+    t.string   "link"
+    t.integer  "coursem_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "semesters", :force => true do |t|
+    t.string   "term",       :null => false
+    t.integer  "year",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
+    t.integer  "karma"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+end
