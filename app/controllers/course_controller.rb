6995d4687ab8ad696855af88b285a80daf7646b7
cp123127 <cp123127@gmail.com>
2013-03-14 02:21:31 -0700

    Moved the heading into application.html.erb from the two html files in course
    Makes for a cleaner interface and will add uniformity to experience with
    more pages to come.

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -5,7 +5,7 @@ class CourseController < ApplicationController
     def index
         # Check for filters
         @courses = Course.getCourseInformation(params[:dept])
-
+        @page_heading = "Browse Courses"
         respond_to do |format|
             format.html
             format.json {render json: @courses }
@@ -17,6 +17,7 @@ class CourseController < ApplicationController
     # GET /course/id.json
     def show
         @course = Course.find(params[:id])
+        @page_heading = @course.name
         respond_to do |format|
             format.html
             format.json {render json: @course }
