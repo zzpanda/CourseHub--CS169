a8907d5accd0a51b792f7424db3e38ccbcb28aa5
Eric <eric@ubuntu.(none)>
2013-03-14 00:47:16 -0700

    Fix: Coursem migration missing course_id and semester_id. Also subscribe and unsubscribe methods

--- db/migrate/20130313224449_create_coursems.rb
+++ db/migrate/20130313224449_create_coursems.rb
@@ -2,6 +2,8 @@ class CreateCoursems < ActiveRecord::Migration
   def change
     create_table :coursems do |t|
         t.string :professor
+        t.integer :course_id
+        t.integer :semester_id
         #id created automatically
         t.timestamps
     end
