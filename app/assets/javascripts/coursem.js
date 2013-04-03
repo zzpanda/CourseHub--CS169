07b8e328fa62938b159e587c8d9d4018ad84be49
cgioia <cgioia@cgioia-VirtualBox.(none)>
2013-04-02 17:10:26 -0700

    halfway done with frontend for view coursem page

---
+++ app/assets/javascripts/coursem.js
@@ -0,0 +1,64 @@
+
+function panelHandler() {
+    $("#button_overview").on("click",function() {
+        showOverview();
+    });
+    $("#button_resources").on("click",function() {
+        showResources();
+    });
+    $("#button_statistics").on("click", function() {
+        showStatistics();
+    });
+}
+
+function showOverview() {
+    $(".panels").hide();
+    $("#panel_description").show();
+    $(".header_button").removeClass("selected");
+    $("#button_overview").addClass("selected");
+}
+
+function showResources() {
+    $(".panels").hide();
+    $("#panel_resources").show();
+    $(".header_button").removeClass("selected");
+    $("#button_resources").addClass("selected");
+}
+
+function showStatistics() {
+    $(".panels").hide();
+    $("#panel_statistics").show();
+    $(".header_button").removeClass("selected");
+    $("#button_statistics").addClass("selected");
+}
+
+function resourceHandler() {
+    /*
+    $(".resource_type table").hide();
+
+    /* Create event handlers for expanding resources upon click */
+    $(".resource_type").each(function(i) {
+        var resource_type = $(this);
+        var title = resource_type.find("h2").html();
+        var expand = resource_type.find(".expand");
+        var table = resource_type.find("table");
+        expand.on("click", function() {
+            table.toggle();
+            if (expand.html() == "+") {
+                expand.html("-");
+            } else {
+                expand.html("+");
+            }
+        });
+
+    });
+    return false;
+}
+
+$(document).ready(function() {
+    panelHandler();
+    //showOverview();
+    showResources();
+    resourceHandler();
+});
+
