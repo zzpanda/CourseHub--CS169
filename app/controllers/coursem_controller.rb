f8e82fd7c23055bbf1cb8a8e39a69efe51e6c450
cgioia <cgioia@cgioia-VirtualBox.(none)>
2013-04-03 15:38:44 -0700

    basic autocomplete , slight changes to navigation

--- app/controllers/coursem_controller.rb
+++ app/controllers/coursem_controller.rb
@@ -9,6 +9,8 @@ class CoursemController < ApplicationController
     # GET /coursem/id.json
     def show
       @coursem = Coursem.getCoursemInformation(params[:id])
+      c = @coursem.course;
+      @page_title = c.department + " " + c.course_number + " : " + c.name;
       if @coursem == ERR_BAD_COURSEM
         respond_to do |format|
           format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
