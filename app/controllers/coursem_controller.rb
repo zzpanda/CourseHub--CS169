217a97e6085bf0b38849b265fc416e319a77af12
Eric <eric@ubuntu.(none)>
2013-04-03 02:12:26 -0700

    Calendar only displays events for current coursem

--- app/controllers/coursem_controller.rb
+++ app/controllers/coursem_controller.rb
@@ -8,12 +8,13 @@ class CoursemController < ApplicationController
     # GET /coursem/id
     # GET /coursem/id.json
     def show
+      @coursem = Coursem.getCoursemInformation(params[:id])
+
       @month = (params[:month] || (Time.zone || Time).now.month).to_i
       @year = (params[:year] || (Time.zone || Time).now.year).to_i
       @shown_month = Date.civil(@year, @month)
-      @event_strips = Event.event_strips_for_month(@shown_month)
+      @event_strips = @coursem.events.event_strips_for_month(@shown_month)
 
-      @coursem = Coursem.getCoursemInformation(params[:id])
       if @coursem == ERR_BAD_COURSEM
         respond_to do |format|
           format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
