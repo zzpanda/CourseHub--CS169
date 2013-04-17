15b9ed59609c20ceaf6aed9eb9ad1d2b49e66912
cp123127 <cp123127@gmail.com>
2013-04-16 21:31:14 -0700

    added railscast url

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
-
+#See here for help on generating this file:
+#http://railscasts.com/episodes/257-request-specs-and-capybara?autoplay=true
 describe "Courses" do
   describe "GET /courses" do
     it "links to signin" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
       visit courses_path
       page.should have_content("sign in")
       page.click_link "sign in"
@@ -15,7 +17,8 @@ describe "Courses" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit courses_path
       page.should have_content("Sign up")
-
+      page.click_link "Sign up"
+      current_path.should == "/users/sign_up"
 
     end
   end
