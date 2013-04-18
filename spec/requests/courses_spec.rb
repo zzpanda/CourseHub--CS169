1abbce55a9806db30ce8274ac391bb4dc5c7d94d
cp123127 <cp123127@gmail.com>
2013-04-18 13:39:21 -0700

    Added initialization to the tests so they don't rely on db:seed

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 #See here for help on generating this file:
 #http://railscasts.com/episodes/257-request-specs-and-capybara?autoplay=true
+# need to run rake db:test:prepare
 describe "Courses" do
-
+  before :each do
+    Course.create(:name => "Software Engineering", :department=> "CS", :course_number=> "169")
+    Course.create(:name => "Linear Algebra", :department=> "Math", :course_number=> "54")
+  end
   describe "GET /courses" do
     it "filters" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
