59210421aa641474cd939701a765fa68c01bcd33
Eric <eric@ubuntu.(none)>
2013-04-19 02:04:30 -0700

    Few working tests for qunit, pushing to serve as examples. Note: to run tests, simply drag html file into browser.

--- test/qunit/coursem_tests.js
+++ test/qunit/coursem_tests.js
@@ -1,6 +1,3 @@
-test('assertions', function() {
-    equal( 1, 1, 'one equals one');
-})
 
 /*
 module ("")
@@ -11,17 +8,58 @@ test ('toggle new resource form', function() {
 
 })
 */
+
+module ("panel and tabs tests", {
+    setup: function() {
+        var tabs = "<div id='tabs'><ul>" +
+            "<li id='button_overview' class='header_button'>Overview</li>" +
+            "<li id='button_resources' class='header_button'>Resources</li>" +
+            "<li id='button_statistics' class='header_button'>Statistics</li>" +
+            "<li id='button_calendar' class='header_button selected'>Calendar</li>" +
+            "</ul></div>";
+        $("#qunit-fixture").append(tabs);
+        $("#qunit-fixture").append("<div id='panel_description' class='panels'>");
+        $("#qunit-fixture").append("<div id='panel_resources' class='panels'>");
+        $("#qunit-fixture").append("<div id='panel_calendar' class='panels'>");
+    }, teardown: function() {
+        $("#qunit-fixture").empty();
+    }
+});
+
+test ("showOverview()", function() {
+    showOverview();
+    equal($('#panel_description').is(':visible'), true, 'the description panel is visible');
+    equal($('#button_overview').hasClass('selected'), true, 'the overview button tab is selected');
+});
+
+test ("showResources()", function() {
+    showResources();
+    equal($('#panel_resources').is(':visible'), true, 'the resources panel is visible');
+    equal($('#button_resources').hasClass('selected'), true, 'the resources button tab is selected');
+});
+
+test ("showCalendar()", function() {
+    showCalendar();
+    equal($('#panel_calendar').is(':visible'), true, 'the calendar panel is visible');
+    equal($('#button_calendar').hasClass('selected'), true, 'the calendar button tab is selected');
+});
+
+
+
+/* Doesn't work because datepicker() and timepicker() not in scope
 module("datetimepickers", {
     setup: function () {
-        $("#qunit-fixture").append("<div id='thing'>hello</div>");
+        $("#qunit-fixture").append("<input id='event_start_date'>");
+        $("#qunit-fixture").append("<input id='event_start_time'>");
+        $("#qunit-fixture").append("<input id='event_end_date'>");
+        $("#qunit-fixture").append("<input id='event_end_time'>");
     },teardown: function() {
         $("#qunit-fixture").empty();
     }
 });
 
-/*
-test("datepicker is called",function() {
-    equal($("#thing").text(), "hello");
-
+test("datetimepicker is called",function() {
+    datetimepickers();
+    assert($("#ui-datepicker-div").exists(), "The datepicker div exists");
 });
 */
