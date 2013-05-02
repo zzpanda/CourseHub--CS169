4460f68865eeb1723dfcab6175a79cc1720890f9
cgioia <cgioia@cgioia-VirtualBox.(none)>
2013-05-02 15:46:43 -0700

    added some registration tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,7 +16,7 @@ describe "Users" do
       page.should have_content("Sign in")
       current_path.should == "/users/sign_in"
     end
-    it "lets users sign up" do
+    it "sign up successfully" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit "/users/sign_in"
       click_link "Sign up"
@@ -29,7 +29,44 @@ describe "Users" do
       current_path.should == "/"
       page.should have_content("Home Page")
     end
-    it "lets users sign in" do
+
+    it 'incorrect registration - no password' do
+      visit "/users/sign_in"
+      click_link "Sign up"
+      fill_in "user_username", :with => "testuser"
+      fill_in "user_email", :with => "testuser@example.com"
+      click_button "Sign up"
+      current_path.should == "/users"
+      page.should have_content("Sign up")
+    end
+
+    it 'incorrect registration - email already taken' do
+      visit "/users/sign_in"
+      click_link "Sign up"
+      fill_in "user_username", :with => "testuser"
+      fill_in "user_email", :with => "test@example.com"
+      fill_in "user_password", :with => "password"
+      fill_in "user_password", :with => "password"
+      click_button "Sign up"
+
+      page.should have_content("Sign up")
+    end
+
+    it 'incorrect registration - passwords do no match' do
+      visit "/users/sign_in"
+      click_link "Sign up"
+      fill_in "user_username", :with => "testuser2"
+      fill_in "user_email", :with => "testuser2@example.com"
+      fill_in "user_password", :with => "password"
+      fill_in "user_password", :with => "pass"
+      click_button "Sign up"
+
+      current_path.should == "/users"
+      page.should have_content("Sign up")
+    end
+
+
+    it "sign in successfully" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit "/users/sign_in"
       fill_in "user_email", :with => "test@example.com"
